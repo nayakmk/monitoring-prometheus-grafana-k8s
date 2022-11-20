b875790d5b9b065a5dddde8dc3557c1851cc4dc5
Malaya Nayak <malaya.nayak@outlook.com>
2022-11-20 19:15:08 +0530

    Monitoring Initial Proj

---
+++ src/test/java/io/nayakmk/samples/monitoring/GradleK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nayakmk.samples.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
